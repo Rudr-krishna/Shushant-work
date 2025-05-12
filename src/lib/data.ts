0b9f8cef5812397d5bffc7e8d2f28e20fd8d729b
Shushant <ss.30072000@gmail.com>
2025-05-12 20:14:10 +0530

    rest pdf

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,18 +19,19 @@ export const works: Work[] = [
     title: 'An Act of a Craven',
     type: 'Short Film',
     genre: 'Psychological Thriller',
-    description: 'This short film script is a psychological thriller exploring guilt, perception, and reality themes. The script was designed for indie filmmakers looking for a production-friendly screenplay with a gripping and atmospheric tone. This is a 2-page excerpt, and the full script is available upon request.',
+    description: 'This short film script is a psychological thriller exploring guilt, perception, and reality themes. The script was designed for indie filmmakers looking for a production-friendly screenplay with a gripping and atmospheric tone. This is a 6-page excerpt, and the full script is available upon request.',
     image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/images1.png',
-    readMoreLink: '#'
+    readMoreLink: 'https://drive.google.com/file/d/1v9oGoN5WIqybRtHHKcJqWxF45M7RtrcW/view?usp=sharing'
+    
   },
   {
     id: '2',
     title: 'The Pool Club',
     type: 'Short Film',
     genre: 'Slice of Life, Drama, Comedy',
-    description: 'I crafted a visually engaging screenplay with minimal dialogue, rich character depth, and a subtle yet powerful emotional arc. This script is perfect for indie filmmakers looking for a simple yet impactful story that resonates with audiences.This is a 2-page excerpt. The full script is available upon request.The dialogues in this script are written in Hindi.',
+    description: 'I crafted a visually engaging screenplay with minimal dialogue, rich character depth, and a subtle yet powerful emotional arc. This script is perfect for indie filmmakers looking for a simple yet impactful story that resonates with audiences.This is a 6-page excerpt. The full script is available upon request.The dialogues in this script are written in Hindi.',
     image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/image2.png',
-    readMoreLink: '#'
+    readMoreLink: 'https://drive.google.com/file/d/1TJM6FuYec3u03Z7wxZHLTXsj_yetAUKY/view?usp=sharing'
   },
   {
     id: '3',
@@ -39,7 +40,7 @@ export const works: Work[] = [
     genre: 'Romance, Drama, Bittersweet',
     description: 'When Vasu learns that his wifes cancer has relapsed and there is nothing more the doctors can do, he refuses to accept defeat. Holding onto hope, he fights against the inevitable, unwilling to let go. But as he sits by her hospital bed, Radha tells him a story—one that intertwines their past, their regrets, and the love they once took for granted.',
     image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/image3.png',
-    readMoreLink: '#'
+    readMoreLink: 'https://drive.google.com/file/d/1HkipboPzB9SERbMw9_52k3fgPr3CP7ji/view?usp=sharing'
   },
   {
     id: '4',
@@ -48,7 +49,7 @@ export const works: Work[] = [
     genre: 'Sci-fi, Thriller',
     description: 'It is a Sci-fi Short story where Fani illegally summons his future selves for a quick task. He expects a shortcut, not blood on the floor and their sudden disappearance. This is an excerpt. The full story is available upon request.',
     image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/image4.png',
-    readMoreLink: '#'
+    readMoreLink: 'https://drive.google.com/file/d/1LmaIID8aN6KbxKG6S1IIxZzXvHhc6CGV/view?usp=sharing'
   },
   {
     id: '5',
@@ -56,8 +57,7 @@ export const works: Work[] = [
     type: 'Feature Idea',
     genre: 'Revenge Tragedy',
     description: 'Two strangers, living miles apart, lead parallel lives. Both have small and happy families. However, their peaceful lives take a dark turn when similar eerie incidents begin to unfold around them and they began to seem more related than mre strangers.',
-    image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/image5.png',
-    readMoreLink: '#'
+    image: 'https://raw.githubusercontent.com/Rudr-krishna/Shushant-work/main/public/images/image5.png'
   }
 ];
 
